6b05df30bfe4f80fd4436d62dc1a02a900bf2e91
William <william.djong@yahoo.com>
2022-03-13 20:35:31 +0700

    Fixed npm run build issue

---
+++ build/precache-manifest.6dca2b2ff62c4b469ae16423c778d402.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9d9bcad6c4c7d3cc52871d89be8a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d7690101f247eca72b",
+    "url": "/static/js/2.65e018fb.chunk.js"
+  },
+  {
+    "revision": "2637d7f593da63114921",
+    "url": "/static/js/main.5bb5294f.chunk.js"
+  },
+  {
+    "revision": "ee3a3661c71eb795ef02",
+    "url": "/static/js/runtime-main.7e3470ec.js"
+  }
+]);
